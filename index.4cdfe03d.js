1287479c1badad2ac812846bc4b173f85b22ef44
Nikita856294 <84923367+Nikita856294@users.noreply.github.com>
2022-11-05 07:06:47 +0000

    Deploying to gh-pages from @ Nikita856294/lawyer-lending-page@fc3dd110fc93eb61e5dcd5847b9bf76c6c24e6c1 🚀

---
+++ index.4cdfe03d.js
@@ -0,0 +1,2 @@
+!function(){var e,t;e=document.querySelector("[data-button]"),t=document.querySelector("[data-mobile-menu]"),e.addEventListener("click",(function(){var n=e.getAttribute("aria-expended");console.log(e),e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.4cdfe03d.js.map
---
+++ index.c95f0530.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-button]"),t=document.querySelector("[data-mobile-menu]");e.addEventListener("click",(()=>{const o=e.getAttribute("aria-expended");console.log(e),e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!o),t.classList.toggle("is-open")}))})();
+//# sourceMappingURL=index.c95f0530.js.map
